e6e15ba51bb602e56081c2daa4edf5ade97aa666
octopus84 <octaviogodoyg@gmail.com>
2022-03-02 21:43:10 -0300

    se crea navbar en template/index.thml

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from Backend.core.views import HomePageView
-from views import(
+from django.urls import include, path
+from . views import(
     HomePageView, 
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',  HomePageView.as_view())
+    path('',  HomePageView.as_view(), name="home"),
+    path('blog/', include('blog.urls',namespace= 'blog')),
 ]
